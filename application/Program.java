09589f166c9790394434d583255c2fe8857e2519
Felipe Faggian <felipesantosfaggian@gmail.com>
2023-05-03 19:37:29 -0300

    Map Exercise

--- application/Program.java
+++ application/Program.java
@@ -1,46 +1,57 @@
 package application;
 
-import java.util.HashSet;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-
-import entities.StudentCode;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Program {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		final Map<String, Integer> mapUrney = new TreeMap<>();
 
-		Set<StudentCode> set = new HashSet<>();
+		System.out.print("Enter file full path: ");
+		String pathFile = sc.nextLine();
+		File sourceFile = new File(pathFile);
 
-		System.out.print("How many students for course A? ");
-		int n = sc.nextInt();
-		
-		for (int i = 0; i<n; i++) {	
-		int number = sc.nextInt();
-		set.add(new StudentCode(number));
+		try (BufferedReader br = new BufferedReader(new FileReader(pathFile))) {
+			String line = br.readLine();
+			while (line != null) {
+				String[] field = line.split(",");
+				String name = field[0];
+				Integer votes = Integer.parseInt(field[1]);
+				if (mapUrney.containsKey(name) == true) {
+					votes = votes + mapUrney.get(name);
+					mapUrney.put(name, votes);
+				} else {
+					mapUrney.put(name, votes);
+				}
+				line = br.readLine();
+			}
+
+		} catch (IOException e) {
+			System.out.println(e);
 		}
+
+		final Map<String, Integer> mapUrneyOrder = mapUrney.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				.collect(Collectors.toMap(
+						Map.Entry::getKey, 
+						Map.Entry::getValue, 
+						(e1, e2) -> e1, LinkedHashMap::new));
 		
-		System.out.print("How many students for course B? ");
-		n = sc.nextInt();
-		
-		for (int i = 0; i<n; i++) {	
-		int number = sc.nextInt();	
-		set.add(new StudentCode(number));
-		}
-		
-		System.out.print("How many students for course C? ");
-		n = sc.nextInt();
-		
-		for (int i = 0; i<n; i++) {		
-		int number = sc.nextInt();	
-		set.add(new StudentCode(number));
-		}
-	
-		System.out.print("Total students: " + set.size());
-		
+		mapUrneyOrder.forEach(((key, value) -> System.out.println(key + ": " + mapUrney.get(key))));
+
 		sc.close();
 	}
 }
